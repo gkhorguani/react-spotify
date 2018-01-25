8626ad38e568a56beac51153074a9a3909716557
Giorgi Khorguani <giorgi.khorguani@proseeder.com>
2018-01-25 14:05:42 -0500

    Spotify auth done

--- src/config/index.js
+++ src/config/index.js
@@ -1,3 +1,1 @@
-export default {
-  client_id: ' 39452579e7c242bc9d4ddfefd815c887'
-};
+export default {};
--- src/modules/api.module.js
+++ src/modules/api.module.js
@@ -1,13 +1,2 @@
 import axios from 'axios';
 import config from 'config';
-
-// Authorization process starts with your application sending a request to the Spotify Accounts service
-export const authorize = (
-  client_id,
-  redirect_uri,
-  state = '',
-  scope,
-  show_dialog = false
-) => {
-  console.log(config);
-};
