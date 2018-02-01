7a9cca586f03e53c630669629da9f26079398750
Giorgi Khorguani <giorgi.khorguani@proseeder.com>
2018-02-01 15:19:39 -0500

    Navigation styling

--- src/schemas/resolvers/profile.resolver.js
+++ src/schemas/resolvers/profile.resolver.js
@@ -1,8 +1,9 @@
 const fetch = require('node-fetch');
 
-export default (root, args, ctx) => fetch('https://api.spotify.com/v1/me', {
-  headers: {
-    Authorization: `Bearer ${ctx.sprat}`,
-  },
-  json: true,
-}).then(res => res.json());
+export default (root, args, ctx) =>
+  fetch('https://api.spotify.com/v1/me', {
+    headers: {
+      Authorization: `Bearer ${ctx.sprat}`,
+    },
+    json: true,
+  }).then(res => res.json());
